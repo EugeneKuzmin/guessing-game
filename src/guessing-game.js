ddc090e6dc3be420c857ab1fc7a9f17323313eb8
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-10-08 18:26:35 +0300

    first commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.gMin = 0
+        this.gMax = 0
+        this.res = 0
+    }
 
     setRange(min, max) {
-
+        this.gMin = min
+        this.gMax = max
     }
 
     guess() {
-
+        this.res =  Math.round((this.gMax-this.gMin)/2)+this.gMin
+        return this.res
     }
 
     lower() {
-
+        this.gMax =  this.res
     }
 
     greater() {
-
+        this.gMin =  this.res
     }
 }
 
